8c8c3f062d9f9c2623292887fad18ffe55d14046
Icaro Gabriel <icarog584@gmail.com>
2026-06-04 14:53:46 -0300

    Creation of the UserService service and creation of the FindAll, FindById methods

--- src/main/java/com/icarogabriel/workshop/repositories/UserRepository.java
+++ src/main/java/com/icarogabriel/workshop/repositories/UserRepository.java
@@ -2,5 +2,7 @@ package com.icarogabriel.workshop.repositories;
 
 import com.icarogabriel.workshop.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> { }
