0651cc5c9e2ac56bce469cf64601643defe40098
Icaro Gabriel <icarog584@gmail.com>
2026-06-06 00:41:53 -0300

    Creation of the OrderRepository repository

---
+++ src/main/java/com/icarogabriel/workshop/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.icarogabriel.workshop.repositories;
+
+import com.icarogabriel.workshop.entities.Order;
+import com.icarogabriel.workshop.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> { }
