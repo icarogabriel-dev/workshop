207833aaeae19bacea12cb2d0c73de694d524949
Icaro Gabriel <icarog584@gmail.com>
2026-06-13 15:15:32 -0300

    Creation of the OrderStatus enum and implementation valueOf method

--- src/main/java/com/icarogabriel/workshop/config/TestConfig.java
+++ src/main/java/com/icarogabriel/workshop/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.icarogabriel.workshop.config;
 
 import com.icarogabriel.workshop.entities.Order;
 import com.icarogabriel.workshop.entities.User;
+import com.icarogabriel.workshop.entities.enums.OrderStatus;
 import com.icarogabriel.workshop.repositories.OrderRepository;
 import com.icarogabriel.workshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +36,9 @@ public class TestConfig implements CommandLineRunner {
         User user2 = new User(null, "Mirella Silva", "mirellaslv@hotmail.com", "11993085127", "silvamirella1");
         userRepository.saveAll(List.of(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2026-02-12T22:41:05Z"), user2);
-        Order order2 = new Order(null, Instant.parse("2026-01-04T01:56:22Z"), user1);
-        Order order3 = new Order(null, Instant.parse("2026-06-25T14:06:39Z"), user2);
+        Order order1 = new Order(null, Instant.parse("2026-02-12T22:41:05Z"), OrderStatus.SHIPPED,user2);
+        Order order2 = new Order(null, Instant.parse("2026-01-04T01:56:22Z"), OrderStatus.PAID, user1);
+        Order order3 = new Order(null, Instant.parse("2026-06-25T14:06:39Z"), OrderStatus.WAITING_PAYMENT, user2);
         orderRepository.saveAll(List.of(order1, order2, order3));
     }
 }
