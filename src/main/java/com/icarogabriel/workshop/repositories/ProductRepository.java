12a0706a0d657fe0a5ea29b55dd86b15a6f1af65
Icaro Gabriel <icarog584@gmail.com>
2026-06-20 18:10:18 -0300

    Creation of the ProductRepository repository

---
+++ src/main/java/com/icarogabriel/workshop/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.icarogabriel.workshop.repositories;
+
+import com.icarogabriel.workshop.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> { }
