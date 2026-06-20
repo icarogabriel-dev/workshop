b09d6a8db1ebb10ff8fffe8f4d10f05514a12ff7
Icaro Gabriel <icarog584@gmail.com>
2026-06-20 18:23:59 -0300

    Added product seeding data to TestConfig class

--- src/main/java/com/icarogabriel/workshop/config/TestConfig.java
+++ src/main/java/com/icarogabriel/workshop/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.icarogabriel.workshop.config;
 
 import com.icarogabriel.workshop.entities.Category;
 import com.icarogabriel.workshop.entities.Order;
+import com.icarogabriel.workshop.entities.Product;
 import com.icarogabriel.workshop.entities.User;
 import com.icarogabriel.workshop.entities.enums.OrderStatus;
 import com.icarogabriel.workshop.repositories.CategoryRepository;
 import com.icarogabriel.workshop.repositories.OrderRepository;
+import com.icarogabriel.workshop.repositories.ProductRepository;
 import com.icarogabriel.workshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     public TestConfig(UserRepository userRepository, OrderRepository orderRepository) {
         this.userRepository = userRepository;
         this.orderRepository = orderRepository;
@@ -50,5 +55,12 @@ public class TestConfig implements CommandLineRunner {
         Category category2 = new Category(null, "Books");
         Category category3 = new Category(null, "Fashion");
         categoryRepository.saveAll(List.of(category1, category2, category3));
+
+        Product product1 = new Product(null, "PC Gamer", "Is a high-performance computer designed to run heavy games with fluidity and superior graphics quality.", 5249.99, "");
+        Product product2 = new Product(null, "Rice", "Is a cereal from the grass family, being the food base of more than half of the world's population.", 10.99, "");
+        Product product3 = new Product(null, "Smart TV", "It is a television with integrated internet that allows you to download applications and browse the web.", 3199.99, "");
+        Product product4 = new Product(null, "Iphone X", "Is Apple's line of high-end smartphones, operating exclusively with the iOS system.", 1559.99, "");
+        Product product5 = new Product(null, "Mirror", "Is a smooth, polished surface, generally composed of flat or curved glass and a metallic layer on the back.", 79.99, "");
+        productRepository.saveAll(List.of(product1, product2, product3, product4, product5));
     }
 }
