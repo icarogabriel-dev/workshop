db0ffd5cbf2958b2e35079c3fc0633a70cc4c4c6
Icaro Gabriel <icarog584@gmail.com>
2026-06-29 19:39:52 -0300

    Establish a one-to-many relationship between the Order and OrderItem entities

--- src/main/java/com/icarogabriel/workshop/entities/OrderItem.java
+++ src/main/java/com/icarogabriel/workshop/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.icarogabriel.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.icarogabriel.workshop.entities.pk.OrderItemPK;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -30,6 +31,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
