1bdad274d0e00af6e4acd8a4bf3a91c2814af39e
Icaro Gabriel <icarog584@gmail.com>
2026-07-05 15:15:19 -0300

    Creation of the Total method in the Order class

--- src/main/java/com/icarogabriel/workshop/entities/Order.java
+++ src/main/java/com/icarogabriel/workshop/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public final boolean equals(Object o) {
         if (!(o instanceof Order order)) return false;
