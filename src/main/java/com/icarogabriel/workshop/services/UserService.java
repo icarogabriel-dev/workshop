57a13b259303d5555a7e0dea2dde1071bab6965a
Icaro Gabriel <icarog584@gmail.com>
2026-07-12 20:34:24 -0300

    Creation of the StandardError class for standardized error response for REST API exceptions

--- src/main/java/com/icarogabriel/workshop/services/UserService.java
+++ src/main/java/com/icarogabriel/workshop/services/UserService.java
@@ -2,7 +2,7 @@ package com.icarogabriel.workshop.services;
 
 import com.icarogabriel.workshop.entities.User;
 import com.icarogabriel.workshop.repositories.UserRepository;
-import exceptions.ResourceNotFoundException;
+import com.icarogabriel.workshop.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/icarogabriel/workshop/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/icarogabriel/workshop/services/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package com.icarogabriel.workshop.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
