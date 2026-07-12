7baf34dffd1e2061d5fd5fb86ca6799011dc6c10
Icaro Gabriel <icarog584@gmail.com>
2026-07-12 20:22:29 -0300

    Creation of the ResourceNotFoundException exception for the FindById method in the UserService

--- src/main/java/com/icarogabriel/workshop/services/UserService.java
+++ src/main/java/com/icarogabriel/workshop/services/UserService.java
@@ -2,6 +2,7 @@ package com.icarogabriel.workshop.services;
 
 import com.icarogabriel.workshop.entities.User;
 import com.icarogabriel.workshop.repositories.UserRepository;
+import exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
