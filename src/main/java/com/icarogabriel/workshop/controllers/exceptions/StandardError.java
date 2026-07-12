5c24055851feb8b66ce91f95f09e46ed4cd74173
Icaro Gabriel <icarog584@gmail.com>
2026-07-12 20:53:25 -0300

    Creation of the ResourceExceptionHandler class for global exception handling

--- src/main/java/com/icarogabriel/workshop/controllers/exceptions/StandardError.java
+++ src/main/java/com/icarogabriel/workshop/controllers/exceptions/StandardError.java
@@ -1,5 +1,7 @@
 package com.icarogabriel.workshop.controllers.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -7,7 +9,9 @@ public class StandardError implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
+
     private Integer status;
     private String error;
     private String message;
