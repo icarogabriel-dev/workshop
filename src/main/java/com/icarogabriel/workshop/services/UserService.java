0698d3481581a2aaa91671af5e8d569fa174082d
Icaro Gabriel <icarog584@gmail.com>
2026-07-18 15:10:40 -0300

    Added exception handling to update method in the UserService

--- src/main/java/com/icarogabriel/workshop/services/UserService.java
+++ src/main/java/com/icarogabriel/workshop/services/UserService.java
@@ -4,6 +4,7 @@ import com.icarogabriel.workshop.entities.User;
 import com.icarogabriel.workshop.repositories.UserRepository;
 import com.icarogabriel.workshop.services.exceptions.DatabaseException;
 import com.icarogabriel.workshop.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
         }
     }
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
