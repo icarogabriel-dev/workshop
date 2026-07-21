34aa65d06e4746c41a43deed15364009bfa42023
Icaro Gabriel <icarog584@gmail.com>
2026-07-20 23:05:15 -0300

    Creation of the endpoints POST, PUT and DELETE in the CategorController controller

--- src/main/java/com/icarogabriel/workshop/controllers/CategoryController.java
+++ src/main/java/com/icarogabriel/workshop/controllers/CategoryController.java
@@ -5,7 +5,9 @@ import com.icarogabriel.workshop.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,30 @@ public class CategoryController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category obj = categoryService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        obj = categoryService.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(obj);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+        obj = categoryService.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 }
