9f35e56d4be13fb9e1b5ffd6baab04529df67508
Icaro Gabriel <icarog584@gmail.com>
2026-07-20 23:02:38 -0300

    Creation of the insert, update and delete methods in the CategoryService service

--- src/main/java/com/icarogabriel/workshop/controllers/CategoryController.java
+++ src/main/java/com/icarogabriel/workshop/controllers/CategoryController.java
@@ -4,10 +4,7 @@ import com.icarogabriel.workshop.entities.Category;
 import com.icarogabriel.workshop.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- src/main/java/com/icarogabriel/workshop/services/CategoryService.java
+++ src/main/java/com/icarogabriel/workshop/services/CategoryService.java
@@ -2,7 +2,11 @@ package com.icarogabriel.workshop.services;
 
 import com.icarogabriel.workshop.entities.Category;
 import com.icarogabriel.workshop.repositories.CategoryRepository;
+import com.icarogabriel.workshop.services.exceptions.DatabaseException;
+import com.icarogabriel.workshop.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,38 @@ public class CategoryService {
 
     public Category findById(Long id) {
         Optional<Category> obj = categoryRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Category insert(Category obj) {
+        return categoryRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+
+        if (!categoryRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            categoryRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category obj) {
+
+        try {
+            Category entity = categoryRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return categoryRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Category entity, Category obj) {
+        entity.setName(obj.getName());
     }
 }
