b9cf4816f83eb4e967a39b0340fe4a5bb827a344
Icaro Gabriel <icarog584@gmail.com>
2026-07-20 23:13:46 -0300

    Creation of the insert, update and delete methods in the ProductService service

--- src/main/java/com/icarogabriel/workshop/services/ProductService.java
+++ src/main/java/com/icarogabriel/workshop/services/ProductService.java
@@ -2,7 +2,11 @@ package com.icarogabriel.workshop.services;
 
 import com.icarogabriel.workshop.entities.Product;
 import com.icarogabriel.workshop.repositories.ProductRepository;
+import com.icarogabriel.workshop.services.exceptions.DatabaseException;
+import com.icarogabriel.workshop.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,41 @@ public class ProductService {
 
     public Product findById(Long id) {
         Optional<Product> obj = productRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Product insert(Product obj) {
+        return productRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+
+        if (!productRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            productRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product obj) {
+
+        try {
+            Product entity = productRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return productRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product obj) {
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setPrice(obj.getPrice());
+        entity.setImgUrl(obj.getImgUrl());
     }
 }
